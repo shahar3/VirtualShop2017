5ad22afee1638e4e01321f1ed08e9d457c03783a
shahar3 <shahar061@gmail.com>
2017-06-03 14:12:45 +0300

    fdgdfg

--- routes/items.js
+++ routes/items.js
@@ -4,9 +4,35 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-    res.send("You are in /items");
+    res.send('in items');
 });
 
-module.exports = router;
+router.get('/getTopFive', function(req, res, next) {
+    res.send('user requested top five');
+});
+
+router.get('/getItems', function(req, res, next) {
+    res.send('user requested items');
+    console.log(req.query);
+});
+
+router.get('/getNewItems', function(req, res, next) {
+    res.send('user requested new items');
+    console.log(req.query);
+});
+
+router.get('/sortBy', function(req, res, next) {
+    res.send('user requested to sort items');
+    console.log(req.query);
+});
+
+router.get('/getItemDetails', function(req, res, next) {
+    res.send('user requested detail on item');
+    console.log(req.query);
+});
+
+router.get('/searchItem', function(req, res, next) {
+    res.send('user searched for item');
+    console.log(req.query);
+});
