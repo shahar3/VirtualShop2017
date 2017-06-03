9fb9018cb5d3015c9de75892124918a2b7303719
yakirhe <yakirhe1@gmail.com>
2017-06-03 16:27:02 +0300

    kijijo

--- dbutils.js
+++ dbutils.js
@@ -26,13 +26,10 @@ function insert(query) {
     console.log("afterConnection");
     connection.on('connect', function (err) {
         if (err) {
-            console.log("error");
             console.log(err);
         } else {
-            console.log("Connected");
             request = new Request(query);
             connection.execSql(request);
-            console.log("after");
         }
     });
 }
